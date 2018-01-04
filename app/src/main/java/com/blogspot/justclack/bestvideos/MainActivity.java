0001884e1aa6b7079ed5394c83512e073f419617
immujahidkhan <immujahidkhan6@gmail.com>
2018-01-05 00:03:51 +0500

    Initial commit

---
+++ app/src/main/java/com/blogspot/justclack/bestvideos/MainActivity.java
@@ -0,0 +1,47 @@
+package com.blogspot.justclack.bestvideos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+  YouTubePlayerView youTubePlayerView;
+  Button button;
+  YouTubePlayer.OnInitializedListener onInitializedListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        youTubePlayerView = findViewById(R.id.YouTubePlayerView);
+
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("suwSV1EIerk");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Toast.makeText(MainActivity.this, "Can't Load", Toast.LENGTH_SHORT).show();
+            }
+        };
+        youTubePlayerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize(PlayerConfig.API_KEY,onInitializedListener);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/blogspot/justclack/bestvideos/PlayerConfig.java
@@ -0,0 +1,11 @@
+package com.blogspot.justclack.bestvideos;
+
+/**
+ * Created by mujahidkhan on 1/4/18.
+ */
+
+public class PlayerConfig {
+    public PlayerConfig() {
+    }
+    public static final String API_KEY = "AIzaSyBsHn_3HBnXXEH92NA7rdkZ8emHQlYetWQ";
+}
